9cfb14b43b69fdc99923777a56053934d33276b7
Geoff M <geoffroym2@gmail.com>
2018-05-11 09:38:41 +0200

    v2.0

--- src/Telecommande.java
+++ src/Telecommande.java
@@ -3,12 +3,12 @@ import java.util.*;
 
 public class Telecommande {
 	private ArrayList<Lampe> lampes;
-	
-	
+	private ArrayList<Hifi> hifis;
+
 	public Telecommande() {
 		// TODO Auto-generated constructor stub
 		lampes = new ArrayList<Lampe>();
-		
+		hifis = new ArrayList<Hifi>();
 	}
 	public void ajouterLampe(Lampe l){
 		lampes.add(l);
@@ -19,23 +19,41 @@ public class Telecommande {
 	public void desactiverLampe(int indiceLampe){
 		lampes.get(indiceLampe).eteindre();
 	}
-	
+
 	public void activerTout(){
 		for(int i=0;i<lampes.size();i++){
 			lampes.get(i).allumer();
 		}
-		
+
+		for(int i=0;i<hifis.size();i++){
+			hifis.get(i).allumer();
+		}
 	}
-	
-	
+
+	public void ajouterHifi(Hifi h){
+		hifis.add(h);
+	}
+	public void activerHifi(int indiceHifi){
+		hifis.get(indiceHifi).allumer();
+	}
+	public void desactiverHifi(int indiceHifi){
+		hifis.get(indiceHifi).eteindre();
+	}
+
+
 
 	public String toString(){
 		String res="";
 		for(int i=0;i<lampes.size();i++){
 			res =res+"Lampe "+i+lampes.get(i).toString();
 		}
+
+		res = res+" || ";
+		for(int i=0;i<hifis.size();i++){
+			res =res+"Hifi "+i+lampes.get(i).toString();;
+		}
 		return res;
-		
+
 	}
 	public Lampe getLampes(int i) {
 		return lampes.get(i);
@@ -48,6 +66,6 @@ public class Telecommande {
 		Lampe l = new Lampe("lampe1");
 		System.out.println("ee"+t.getLampes(0));
 	}
-	
+
 
 }
